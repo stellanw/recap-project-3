03861e2b86def60c35413f3fef993c8b887aecf1
Stellan Wetzig <stellanwetzig@yahoo.de>
2024-01-31 15:46:05 +0100

    fetch data

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,29 +1,29 @@
 const cardContainer = document.querySelector('[data-js="card-container"]');
 
-export function CharacterCard() {
-    const newCard = document.createElement("li");
-    newCard.innerHTML = `
-    <li class="card">
-      <div class="card__image-container">
-        <img
-          class="card__image"
-          src=${}
-          alt=${}
-        />
-        <div class="card__image-gradient"></div>
-      </div>
-      <div class="card__content">
-        <h2 class="card__title">${}</h2>
-        <dl class="card__info">
-          <dt class="card__info-title">Status</dt>
-          <dd class="card__info-description">${}</dd>
-          <dt class="card__info-title">Type</dt>
-          <dd class="card__info-description">${}</dd>
-          <dt class="card__info-title">Occurrences</dt>
-          <dd class="card__info-description">${}</dd>
-        </dl>
-      </div>
-    </li>
-`;
-    cardContainer.append(newCard);
+export function CharacterCard(character) {
+  const newCard = document.createElement("li");
+  newCard.innerHTML = `
+      <li class="card">
+        <div class="card__image-container">
+          <img
+            class="card__image"
+            src=${character.image}
+            alt=${character.name}
+          />
+          <div class="card__image-gradient"></div>
+        </div>
+        <div class="card__content">
+          <h2 class="card__title">${character.name}</h2>
+          <dl class="card__info">
+            <dt class="card__info-title">Status</dt>
+            <dd class="card__info-description">${character.status}</dd>
+            <dt class="card__info-title">Type</dt>
+            <dd class="card__info-description">${character.type}</dd>
+            <dt class="card__info-title">Occurrences</dt>
+            <dd class="card__info-description">${character.episode}</dd>
+          </dl>
+        </div>
+      </li>
+  `;
+  cardContainer.append(newCard);
 }
