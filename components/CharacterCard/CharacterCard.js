11b3a7134c88b6e9b8459544115b8546a6bd9339
Anna von Oesen <155978924+avoesen@users.noreply.github.com>
2024-01-31 15:03:40 +0100

    add characterCard()

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,1 +1,29 @@
-export function CharacterCard() {}
+const cardContainer = document.querySelector('[data-js="card-container"]');
+
+export function CharacterCard() {
+    const newCard = document.createElement("li");
+    newCard.innerHTML = `
+    <li class="card">
+      <div class="card__image-container">
+        <img
+          class="card__image"
+          src=${}
+          alt=${}
+        />
+        <div class="card__image-gradient"></div>
+      </div>
+      <div class="card__content">
+        <h2 class="card__title">${}</h2>
+        <dl class="card__info">
+          <dt class="card__info-title">Status</dt>
+          <dd class="card__info-description">${}</dd>
+          <dt class="card__info-title">Type</dt>
+          <dd class="card__info-description">${}</dd>
+          <dt class="card__info-title">Occurrences</dt>
+          <dd class="card__info-description">${}</dd>
+        </dl>
+      </div>
+    </li>
+`;
+    cardContainer.append(newCard);
+}
